187745b43acafca9fe719afeb738b72193f3cfbb
davik20 <onyejivic@gmail.com>
2023-06-01 19:30:03 +0100

    add lower case feature

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod camel_case;
+pub mod capitalize;
+pub mod difference;
+
+pub mod lower_case;
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+
+use std::prelude;
+
+
+extern crate lodash_rust;
+fn main(){
+   println!("{:?}",lodash_rust::lower_case::from_str("DAVoik"));
+}
